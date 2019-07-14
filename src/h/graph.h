1aca686145ce5a9287d7dce3ee2e7b21913ddf78
Mandrenkov <M.Andrenkov@gmail.com>
2019-07-13 23:14:46 -0400

    Added the graph::outgoing() function which returns a list of Edges that share a common head Node.

--- src/h/graph.h
+++ src/h/graph.h
@@ -15,11 +15,12 @@ namespace ctgl {
             static constexpr int id = ID;
         };
 
-        // Edge represents a directed edge from the Node |F| to the Node |T| with positive weight |W|.
-        template<typename F, typename T, int W = 1, typename = std::enable_if_t<W >= 0>>
+        // Edge represents a directed edge from the tail Node |T| to the head
+        // Node |H| with positive weight |W|.
+        template<typename T, typename H, int W = 1, typename = std::enable_if_t<W >= 0>>
         struct Edge {
-            using From = F;
-            using To = T;
+            using Tail = T;
+            using Head = H;
             static constexpr int weight = W;
         };
 
@@ -37,6 +38,10 @@ namespace ctgl {
         // Finds all Nodes connected to the given Node in the provided Graph.
         template <typename G, typename N>
         constexpr auto connected(G, N) noexcept;
+
+        // Finds all outgoing Edges from the given Node in the provided Graph.
+        template <typename G, typename N>
+        constexpr auto outgoing(G, N) noexcept;
     }
 
     // Definitions
@@ -48,14 +53,14 @@ namespace ctgl {
             return list::unique(adjacent(N{}, typename G::Edges{}));
         }
 
-        template <typename N, typename T, int W, typename... Es>
-        constexpr auto adjacent(N, List<Edge<N, T, W>, Es...>) noexcept {
+        template <typename N, typename H, int W, typename... Es>
+        constexpr auto adjacent(N, List<Edge<N, H, W>, Es...>) noexcept {
             // The first Edge in the List originates from the source Node.
-            return T{} + adjacent(N{}, List<Es...>{});
+            return H{} + adjacent(N{}, List<Es...>{});
         }
 
-        template <typename N, typename F, typename T, int W, typename... Es>
-        constexpr auto adjacent(N, List<Edge<F, T, W>, Es...>) noexcept {
+        template <typename N, typename E, typename... Es>
+        constexpr auto adjacent(N, List<E, Es...>) noexcept {
             // The first Edge in the List does NOT originate from the source Node.
             return adjacent(N{}, List<Es...>{});
         }
@@ -70,27 +75,50 @@ namespace ctgl {
         constexpr auto connected(G, N) noexcept {
             constexpr bool feasible = list::contains(N{}, typename G::Nodes{});
             if constexpr (feasible) {
-                return list::unique(N{} + connected(G{}, N{}, adjacent(G{}, N{})));
+                constexpr auto next = adjacent(G{}, N{});
+                constexpr auto span = connected(G{}, N{}, next, List<N>{});
+                return list::unique(span);
             } else {
                 return List<>{};
             }
         }
 
-        template <typename G, typename N, typename T, typename... Ts, typename = ctgl::detail::enable_if_different_t<N, T>>
-        constexpr auto connected(G, N, List<T, Ts...>) noexcept {
-            // The first Node in the adjacency List connects to a Node other than the target Node.
-            return T{} + connected(G{}, N{}, List<Ts...>{}) + connected(G{}, T{}, adjacent(G{}, T{}));
+        template <typename G, typename N, typename T, typename... Ts, typename... Ps>
+        constexpr auto connected(G, N, List<T, Ts...>, List<Ps...>) noexcept {
+            constexpr auto skip = connected(G{}, N{}, List<Ts...>{}, List<Ps...>{});
+            constexpr auto cycle = list::contains(T{}, List<Ps...>{});
+            if constexpr (cycle) {
+                return skip;
+            } else {
+                constexpr auto next = adjacent(G{}, T{});
+                constexpr auto take = connected(G{}, T{}, next, List<T, Ps...>{});
+                return skip + take;
+            }
         }
 
-        template <typename G, typename N, typename... Ts>
-        constexpr auto connected(G, N, List<N, Ts...>) noexcept {
-            // The first Node in the adjacency List loops back to the target Node.
-            return connected(G{}, N{}, List<Ts...>{});
+        template <typename G, typename N, typename... Ps>
+        constexpr auto connected(G, N, List<>, List<Ps...>) noexcept {
+            return List<Ps...>{};
         }
 
         template <typename G, typename N>
-        constexpr auto connected(G, N, List<>) noexcept {
-            // All the Edges have been traversed.
+        constexpr auto outgoing(G, N) noexcept {
+            return list::unique(outgoing(N{}, typename G::Edges{}));
+        }
+
+        template <typename N, typename E, typename... Es>
+        constexpr auto outgoing(N, List<E, Es...>) noexcept {
+            constexpr bool match = std::is_same_v<N, typename E::Tail>;
+            constexpr auto after = outgoing(N{}, List<Es...>{});
+            if constexpr (match) {
+                return E{} + after;
+            } else {
+                return after;
+            }
+        }
+
+        template <typename N>
+        constexpr auto outgoing(N, List<>) noexcept {
             return List<>{};
         }
     }
@@ -100,8 +128,8 @@ namespace ctgl {
     template <int ID>
     using Node = ctgl::graph::Node<ID>;
 
-    template<typename F, typename T, int W>
-    using Edge = ctgl::graph::Edge<F, T, W>;
+    template<typename T, typename H, int W>
+    using Edge = ctgl::graph::Edge<T, H, W>;
 
     template <typename N, typename E>
     using Graph = ctgl::graph::Graph<N, E>;
--- src/test/graph_test.cpp
+++ src/test/graph_test.cpp
@@ -3,17 +3,21 @@
 #include "../h/graph.h"
 #include "forge.h"
 
+#include <iostream>
+
 using namespace ctgl;
 
 
 // Convenient Type Aliases
 // -----------------------------------------------------------------------------
 template <typename G, typename N>
-using AdjacentType = decltype(ctgl::graph::adjacent(G{}, N{}));
+using AdjacentType = decltype(graph::adjacent(G{}, N{}));
 
 template <typename G, typename N>
-using ConnectedType = decltype(ctgl::graph::connected(G{}, N{}));
+using ConnectedType = decltype(graph::connected(G{}, N{}));
 
+template <typename G, typename N>
+using OutgoingType = decltype(graph::outgoing(G{}, N{}));
 
 // Unit Tests
 // -----------------------------------------------------------------------------
@@ -37,7 +41,7 @@ TEST(GraphTest, Adjacent) {
     EXPECT_TRUE((std::is_same<AdjacentType<forge::Bridge, forge::N1>, List<forge::N2>>::value));
     EXPECT_TRUE((std::is_same<AdjacentType<forge::Bridge, forge::N2>, List<forge::N1>>::value));
 
-    // Triangle
+    // Leap
     EXPECT_TRUE((std::is_same<AdjacentType<forge::Leap, forge::N1>, List<forge::N2, forge::N3>>::value));
     EXPECT_TRUE((std::is_same<AdjacentType<forge::Leap, forge::N2>, List<forge::N3>>::value));
     EXPECT_TRUE((std::is_same<AdjacentType<forge::Leap, forge::N3>, List<>>::value));
@@ -56,8 +60,39 @@ TEST(GraphTest, Connected) {
     EXPECT_TRUE((std::is_same<ConnectedType<forge::Loopback, forge::N1>, List<forge::N1>>::value));
 
     // Pan
-    EXPECT_TRUE((std::is_same<ConnectedType<forge::Pan, forge::N1>, List<forge::N1, forge::N4, forge::N2, forge::N3>>::value));
-    EXPECT_TRUE((std::is_same<ConnectedType<forge::Pan, forge::N2>, List<           forge::N2, forge::N3           >>::value));
-    EXPECT_TRUE((std::is_same<ConnectedType<forge::Pan, forge::N3>, List<                      forge::N3           >>::value));
-    EXPECT_TRUE((std::is_same<ConnectedType<forge::Pan, forge::N4>, List<forge::N4, forge::N2, forge::N3>>::value));
+    EXPECT_TRUE((std::is_same<ConnectedType<forge::Pan, forge::N1>, List<forge::N4, forge::N3, forge::N2, forge::N1>>::value));
+    EXPECT_TRUE((std::is_same<ConnectedType<forge::Pan, forge::N2>, List<forge::N3, forge::N2>>::value));
+    EXPECT_TRUE((std::is_same<ConnectedType<forge::Pan, forge::N3>, List<forge::N3>>::value));
+    EXPECT_TRUE((std::is_same<ConnectedType<forge::Pan, forge::N4>, List<forge::N3, forge::N2, forge::N4>>::value));
+
+    // Triangle
+    EXPECT_TRUE((std::is_same<ConnectedType<forge::Triangle, forge::N1>, List<forge::N3, forge::N2, forge::N1>>::value));
+    EXPECT_TRUE((std::is_same<ConnectedType<forge::Triangle, forge::N2>, List<forge::N1, forge::N3, forge::N2>>::value));
+    EXPECT_TRUE((std::is_same<ConnectedType<forge::Triangle, forge::N3>, List<forge::N2, forge::N1, forge::N3>>::value));
+}
+
+// Tests for the outgoing() function.
+TEST(GraphTest, Outgoing) {
+    // Empty
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Empty, forge::N1>, List<>>::value));
+
+    // Island
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Empty, forge::N1>, List<>>::value));
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Empty, forge::N2>, List<>>::value));
+
+    // Loopback
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Loopback, forge::N1>, List<forge::E11>>::value));
+
+    // Arrow
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Arrow, forge::N1>, List<forge::E12>>::value));
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Arrow, forge::N2>, List<>>::value));
+
+    // Bridge
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Bridge, forge::N1>, List<forge::E12>>::value));
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Bridge, forge::N2>, List<forge::E21>>::value));
+
+    // Leap
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Leap, forge::N1>, List<forge::E12, forge::E13>>::value));
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Leap, forge::N2>, List<forge::E23>>::value));
+    EXPECT_TRUE((std::is_same<OutgoingType<forge::Leap, forge::N3>, List<>>::value));
 }
